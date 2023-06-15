33f677c62e4234656f877ca5f155a79b6dff6953
AnLRP <lethanhan342004@gmail.com>
2023-06-15 12:09:46 +0700

    Add project files.

---
+++ ASP.NetMVC_LapTrinhWebsiteThuongMaiCoBan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NetMVC_LapTrinhWebsiteThuongMaiCoBan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
